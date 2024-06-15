3c54197ad0dd10448e1bc60fbc66d5a69da32b6b
buhtanback <indamix31@gmail.com>
2024-06-15 14:41:04 +0300

    correct work with mesh

--- Blueto_bridge.ino
+++ Blueto_bridge.ino
@@ -1,80 +1,34 @@
-//
-// nodeId = 624409705
-//
 #include "painlessMesh.h"
 #include "BluetoothSerial.h"
 
+#define MESH_PREFIX     "buhtan"
+#define MESH_PASSWORD   "buhtan123"
 
-#define   MESH_PREFIX     "kennet"
-#define   MESH_PASSWORD   "kennet123"
-
-painlessMesh  mesh;
+painlessMesh mesh;
 BluetoothSerial SerialBT;
 
-// bool garlandS;
-
-// bool redledP;
-// int modeR;
-
-// unsigned long pM = 0; // Змінна для зберігання часу останньої відправки
-// const int interval = 10; // Інтервал затримки в мілісекундах
-
-void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
-  if(event == ESP_SPP_SRV_OPEN_EVT){
-    Serial.println("Client Connected");
-    SerialBT.print("hello");
-
-    // unsigned long cM = millis();
-
-    // if (cM - pM >= interval) {
-    //   pM = cM; // Зберігання часу останньої відправки
-      
-    //   mesh.sendSingle(2224853816,"garland_echo");
-    //   pM = millis(); 
-    //   mesh.sendSingle(624315197,"red_led_echo");
-    //   pM = millis();
-    //   mesh.sendSingle(635035530,"bedside_echo");
-    //   pM = millis();
-    //   mesh.sendSingle(985208077,"sens_echo");
-    // }
-  }
-
- 
-  if(event == ESP_SPP_CLOSE_EVT ){            //ребут при дісконекті
-    Serial.println("Client disconnected");
-    ESP.restart();
-  }
-}
-
-void receivedCallback( uint32_t from, String &msg ) {
+void receivedCallback(uint32_t from, String &msg) {
   String str1 = msg.c_str();
   SerialBT.print(str1);
 }
 
-
 void setup() {
   Serial.begin(115200);
-  //Serial.print("start");
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD );
+  mesh.init(MESH_PREFIX, MESH_PASSWORD);
   mesh.onReceive(&receivedCallback);
 
-  SerialBT.register_callback(callback);
-
-  SerialBT.begin("Kennet'MASH'"); 
+  SerialBT.begin("pies'MASH'");
+  Serial.println("The device started, now you can pair it with Bluetooth!");
 }
 
-
 void loop() {
-
   mesh.update();
 
+  // Передача даних з Bluetooth в mesh
   if (SerialBT.available()) {
-
-    String str = (SerialBT.readString());
+    String str = SerialBT.readString();
     str.trim();
-    //Serial.print (str);
-
     mesh.sendBroadcast(str);
   }
 }
